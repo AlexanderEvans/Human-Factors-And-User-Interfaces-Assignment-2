1b1e9840a0fca1d6285390c07addb9bd6195d68d
AlexanderEvans <AlexanderEvans@users.noreply.github.com>
2019-02-20 11:15:17 -0600

    Initial Commit

---
+++ store.py
@@ -0,0 +1,21 @@
+#**********************************************************************
+#****      Student: Alexander Evans                                ****
+#****        Class: Human Factors and User Interfaces              ****
+#****   Instructor: Gamradt                                        ****
+#****   Assignment: 2                                              ****
+#****     Due Date: 2/9/2019                                       ****
+#**********************************************************************
+#****  Description:                                                ****
+#**********************************************************************
+import tkinter as tk
+from tkinter import ttk
+#additional module imports
+
+#detailed function description blocks for each defined function
+
+#Create "top level" window
+root = tk.Tk()
+myLabel = tk.Label(root, text="Hello World")
+myLabel.pack()
+#Start the event loop
+root.mainloop();
