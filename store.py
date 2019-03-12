5fc48b70a283ed6aa24b48b0ec4349db72b97eea
AlexanderEvans <AlexanderEvans@users.noreply.github.com>
2019-03-12 05:16:56 -0500

    implemented menubar

--- store.py
+++ store.py
@@ -27,8 +27,6 @@ root.mainloop();
 def makeMenu(parent):
     menubar = tk.Frame(parent)
     menubar.pack(side = 'top', fill = 'x')
-    btnFile = tk.Menubutton(menubar, text = 'File', underline = 0)
-    btnFile.pack(side='left')
 
 
 class ProductFrame(tk.Frame):
