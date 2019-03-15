33898a1ca94f4ee005debf82c9a1d0c7e74c50b9
AlexanderEvans <AlexanderEvans@users.noreply.github.com>
2019-03-15 09:59:23 -0500

    fix stuff

--- totalframe.py
+++ totalframe.py
@@ -4,10 +4,17 @@ from tkinter import ttk
 class TotalFrame(tk.Frame):
     def __init__(self, parent = None, total = 0, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
-
+        
         self.costTotal = tk.Label(self, text = "Subtotal")
         self.cost = repr(total)
         self.lcost = tk.Label(self, text = self.cost)
+
+        self.DiscountTotal = tk.Label(self, text = "Discount")
+        
+        self.dis = "Discounted"
+        if total<10.00:
+            self.dis = "No discount!"
+        self.ldis = tk.Label(self, text = self.cost)
         
         self.costTotal.grid(row = 0, column = 0)
         self.lcost.grid(row = 0, column = 1)
@@ -25,3 +32,5 @@ class TotalFrame(tk.Frame):
         
         self.taxTot.grid(row = 2, column = 0)
         self.ltaxTot.grid(row = 2, column = 1)
+        self.DiscountTotal.grid(row = 3, column = 0)
+        self.ldis.grid(row = 3, column = 1)
