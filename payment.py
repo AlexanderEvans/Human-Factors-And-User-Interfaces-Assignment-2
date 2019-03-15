ad980e94bcb4753f9fb10f4ce1a552f4dab73d27
AlexanderEvans <AlexanderEvans@users.noreply.github.com>
2019-03-15 08:10:23 -0500

    seperated credential frame class into file

--- payment.py
+++ payment.py
@@ -1,34 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
-
-class CredentialFrame(tk.Frame):
-    def __init__(self, parent = None, reciept = "", total = 0, *args, **kwargs):
-        super().__init__(parent, *args, **kwargs)
-        self.First = tk.Entry(self)
-        self.Last = tk.Entry(self)
-        self.adress = tk.Entry(self)
-        self.ccn = tk.Entry(self)
-        self.ccv = tk.Entry(self)
-        self.LFirst = tk.Label(self, text = "First Name: ")
-        self.LLast = tk.Label(self, text = "Last Name: ")
-        self.Ladress = tk.Label(self, text = "Adress: ")
-        self.Lccn = tk.Label(self, text = "Credit Card Number: ")
-        self.Lccv = tk.Label(self, text = "CCV: ")
-        
-        self.First.grid(row = 0, column = 1)
-        self.LFirst.grid(row = 0, column = 0)
-
-        self.Last.grid(row = 2, column  = 1)
-        self.LLast.grid(row = 2, column = 0)
-
-        self.adress.grid(row = 3, column = 1)
-        self.Ladress.grid(row = 3, column = 0)
-
-        self.ccn.grid(row = 4, column = 1)
-        self.Lccn.grid(row = 4, column = 0)
-
-        self.ccv.grid(row = 5, column = 1)
-        self.Lccv.grid(row = 5, column = 0)
+import credentialframe
 
 class RecieptFrame(tk.Frame):
     def __init__(self, parent = None, reciept = "", *args, **kwargs):
@@ -67,7 +39,7 @@ class TotalFrame(tk.Frame):
 class Payment(tk.Toplevel):
     def __init__(self, parent = None, reciept = "", total = 0, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
-        self.credentialFrame = CredentialFrame(self)
+        self.credentialFrame = credentialframe.CredentialFrame(self)
         self.credentialFrame.grid(row = 0, column = 0, columnspan = 2)
         self.items = RecieptFrame(self, reciept)
         self.totals = TotalFrame(self, total)
