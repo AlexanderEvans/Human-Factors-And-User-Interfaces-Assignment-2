5d8ffde6550ad76bf3d00b0bbe41e253caac9424
AlexanderEvans <AlexanderEvans@users.noreply.github.com>
2019-03-15 08:12:15 -0500

    refactored payment class

--- payment.py
+++ payment.py
@@ -33,12 +33,13 @@ class Payment(tk.Toplevel):
     def __init__(self, parent = None, reciept = "", total = 0, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
         self.credentialFrame = credentialframe.CredentialFrame(self)
-        self.credentialFrame.grid(row = 0, column = 0, columnspan = 2)
         self.items = recieptframe.RecieptFrame(self, reciept)
         self.totals = TotalFrame(self, total)
+        self.btn = tk.Button(self, command = self.quit, text = "Place Order")
+
+        self.credentialFrame.grid(row = 0, column = 0, columnspan = 2)
         self.items.grid(row = 1, column = 0)
         self.totals.grid(row = 1, column = 2)
-        self.btn = tk.Button(self, command = self.quit, text = "Place Order")
         self.btn.grid(row = 2, column = 0, columnspan = 2)
 
 
